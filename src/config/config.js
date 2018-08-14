c21a84997b48feb044397bc816e00fed8a31fbde
olegbest <pashkevich.oleg@inbox.ru>
2018-08-14 18:01:29 +0300

    123

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 8081,
-    dbURL: 'mongodb://localhost:27017/users',
+    dbURL: 'mongodb://localhost:27017/HearHeroUsers',
     dbOptions: {
         useNewUrlParser: true
     }
