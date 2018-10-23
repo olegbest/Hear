f6e0816d582ea6296e9365ec0b5621986c1aa361
olegbest <pashkevich.oleg@inbox.ru>
2018-10-23 16:50:06 +0300

    connect auth to DB

--- src/config/config.js
+++ src/config/config.js
@@ -3,10 +3,7 @@ module.exports = {
     port: 8081,
     dbURL: 'mongodb://localhost:27017/HearHeroDB',
     dbOptions: {
-        useNewUrlParser: true,
-        auth: {authSource: "admin"},
-        user: cfg.user,
-        pass: cfg.pass
+        useNewUrlParser: true
     }
 
 };
