43b64fd2193ec5128167c210b95ecf251df71f61
olegbest <pashkevich.oleg@inbox.ru>
2018-10-23 16:58:28 +0300

    connect auth to DB

--- src/libs/mongoose.js
+++ src/libs/mongoose.js
@@ -1,38 +1,6 @@
-let mongoose    = require('mongoose');
+let mongoose = require('mongoose');
 let config = require('./../config/config');
-
 
 
 mongoose.createConnection(config.dbURL, config.dbOptions);
 
-var db = mongoose.connection;
-
-
-
-//db.dropDatabase();
-
-db.on('error', function (err) {
-    console.log('connection error:' + err.message);
-});
-db.once('open', function callback () {
-    console.log("db opened");
-});
-
-const Schema = mongoose.Schema;
-const PostSchema = new Schema({
-    username: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    email : {
-        type: String
-    },
-    data: {
-        type: Object
-    }
-
-});
-const PostModel = mongoose.model('post', PostSchema);
-module.exports.PostModel = PostModel;
